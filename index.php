f8c843e2bddc13a1906242b25cd1343cf3fb17fb
lilmanR <hilman.hfh@gmail.com>
2024-05-11 10:46:07 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$link = new PDO("mysql:host=localhost;dbname=db_oopnvc", "root", "");
+$result = $link ->query("SELECT * from anggota");
+
+$anggota = array();
+while ($row = $result->fetch(PDO::FETCH_ASSOC)){
+    $anggota[]=$row;
+}
+
+$link = null;
+require "view/anggota/list.php";
+?>
